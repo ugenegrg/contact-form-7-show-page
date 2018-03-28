ad0b40a6298db9ae363a6827f3a1fede614f6afa
ugene <ugenegrg@gmail.com>
2018-03-28 16:40:42 +0545

    push from hps tets env

---
+++ contact-form-7-show-page.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Plugin Name: Contact Form 7 - Show Page
+ * Description: An add-on for Contact Form 7 that shows the all the post types where the contact form is being used.
+ * Version: 1.0.2
+ * Author: Sachyya, Ugene
+ * Author URI:
+ * License: GPLv3
+ */
+
+/**
+ * Show admin notice on CF7 dependency.
+ * @return string Notice about the plugin dependency.
+ */
+function wpcf7sp_admin_notice() {
+	// Verify that CF7 is active and updated to the required version (currently 3.9.0)
+	if ( ! is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {
+		echo '<div class="error"><p>Contact Form 7 is not activated. The Contact Form 7 plugin must be installed and activated before you can use Contact Form 7 - Show Page plugin.</p></div>';
+	}
+}
+add_action( 'admin_notices', 'wpcf7sp_admin_notice' );
+
+/**
+ * Load style file
+ */
+function wpcf7sp_load_style() {
+	wp_enqueue_style( 'wpcf7sp_style', plugins_url( '/css/show-page.css', __FILE__ ), '', time(), 'all' );
+}
+add_action( 'admin_footer', 'wpcf7sp_load_style' );
+
+
+/**
+ * Add extra panel to form editor.
+ *
+ * @param  Array $panels Array form editor panel args.
+ *
+ * @return Array $panels  Array form editor panel args.
+ */
+function wpcf7sp_add_panel( $panels ) {
+	$post = wpcf7_get_current_contact_form();
+	if ( current_user_can( 'wpcf7_edit_contact_form',  $post->id() ) ) {
+		$panels['cf7-show-page-panel'] = array(
+			'title'    => __( 'CF7 Show Pages', 'contact-form-7' ),
+			'callback' => 'wpcf7sp_editor_panel_cf7_show_page'
+		);
+	}
+
+	return $panels;
+}
+add_filter( 'wpcf7_editor_panels', 'wpcf7sp_add_panel' );
+
+/**
+ * Show the panel itself
+ *
+ */
+function wpcf7sp_editor_panel_cf7_show_page( $post ) { ?>
+    <div class="wpcf7sp_panel">
+
+        <h2><?php echo esc_html( __( 'Contact Form 7 - Show Page', 'wpcf7sp' ) ); ?></h2>
+        <h4><?php _e( 'Lists of post types using this form.', 'wpcf7sp' ); ?></h4>
+
+    	<?php
+    	$wpcf7sp_form_id       = $post->id();
+    	$wpcf7sp_search_string = 'contact-form-7 id="' . $wpcf7sp_form_id . '"';
+    	$wpcf7sp_posttypes     = apply_filters( 'wpcf7sp_support_posttypes', get_post_types( array( 'public' => true ) ) );
+
+    	if ( ! ( is_array( $wpcf7sp_posttypes ) || is_string( $wpcf7sp_posttypes ) ) ) {
+    		$wpcf7sp_posttypes = get_post_types( array( 'public' => true ) );
+    	}
+
+    	$post_query = new WP_Query( array(
+    		's'         => $wpcf7sp_search_string,
+    		'post_type' => $wpcf7sp_posttypes
+    	) );
+
+    	if ( $post_query->have_posts() ) { ?>
+            <table class="wpcf7sp">
+                <tbody>
+                <tr>
+                    <th width="55%"><?php _e( 'Title', 'wpcf7sp' ); ?></th>
+                    <th width="25%"><?php _e( 'Post Type', 'wpcf7sp' ); ?></th>
+                    <th width="20%"></th>
+                </tr>
+    			<?php
+    			while ( $post_query->have_posts() ) {
+    				$post_query->the_post();
+    				$post_type = get_post_type();
+
+    				?>
+                    <tr>
+                        <td width="50%"><?php the_title(); ?></td>
+                        <td width="25%"><?php echo $post_type; ?></td>
+                        <td width="25%" class="wpcf7sp-opt">
+                            <a href="<?php the_permalink(); ?>" class="wpcf7sp-alink" target="_blank">View</a>
+                            <a href="<?php echo get_edit_post_link( get_the_ID() ); ?>" class="wpcf7sp-alink"
+                               target="_blank">Edit</a>
+                        </td>
+                    </tr>
+    				<?php
+    			}
+    			?>
+                </tbody>
+            </table>
+    		<?php
+    	} else { ?>
+            <small><?php _e( 'No posts or pages use this form right now.', 'wpcf7sp' ); ?></small>
+    	<?php } ?>
+
+        <h4><?php _e( 'Lists of Text widgets using this form.', 'wpcf7sp' ); ?></h4>
+    	<?php
+    	// Query for the text widgets with the contact form.
+    	global $wpdb;
+    	// Get prefixed table name.
+    	$wpcf7sp_table = $wpdb->prefix . 'options';
+    	// Prepare query which searches for option name with widget_text and also verify for the empty and no form used condition.
+    	$wpcf7sp_widget_query   = $wpdb->prepare( "SELECT * FROM `$wpcf7sp_table` WHERE `option_name` LIKE '%%widget_text%%' AND `option_value` LIKE '%%%s%%'", $wpcf7sp_search_string );
+    	$wpcf7sp_widget_results = $wpdb->get_results( $wpcf7sp_widget_query );
+        // var_dump( $wpcf7sp_widget_results );
+
+    	/******************************* if case should be applied right here **********************************************
+    	* because no need to get zero result
+    	* and unserialize it
+    	* loop it
+    	* and regex it for $widget_ids
+    	* and again query for sidebar
+    	* and again loop sidebar results to check $widget_ids
+    	********************************************************************************************************************/
+        if( !empty( $wpcf7sp_widget_results ) ) {
+
+        	$wpcf7sp_widget_results = $wpcf7sp_widget_results[0];
+        	// Unserialize data which contains all the text widgets found.
+        	$wpcf7sp_text_widgets = maybe_unserialize( $wpcf7sp_widget_results->option_value );
+
+        	// Prepare search string to be searched in result.
+        	$serach_expression = '/\[' . $wpcf7sp_search_string . '/';
+        	$widget_ids        = [];
+        	foreach ( $wpcf7sp_text_widgets as $key => $value ) {
+        		if ( is_array( $value ) ) {
+        			// Check if the value is set and not empty and match with the search expression.
+        			if ( isset( $value['text'] ) && ! empty( $value['text'] ) && preg_match( $serach_expression, $value['text'] ) ) {
+        				// Append 'text-' to make it comparable in later in_array condition.
+        				$widget_ids[] = 'text-' . $key;
+        			}
+        		}
+        	}
+
+        	// Query for the sidebars with the above found text widgets.
+        	$widget_resp_sidebar_query = $wpdb->prepare( "SELECT * FROM `$wpcf7sp_table` WHERE `option_name` = 'sidebars_widgets'", $wpcf7sp_search_string );
+        	$widget_resp_sidebar_res   = $wpdb->get_results( $widget_resp_sidebar_query );
+        	// Unserialize data which contains all the sidebars.
+        	$widget_resp_sidebar_res = maybe_unserialize( $widget_resp_sidebar_res[0]->option_value );
+
+        	$side_bar_ids = [];
+        	foreach ( $widget_resp_sidebar_res as $key => $value ) {
+        		if ( ! empty( $value ) && is_array( $value ) ) {
+        			for ( $i = 0; $i < count( $value ); $i ++ ) {
+        				// Check if the above widget_ids is in the recently found array.
+        				if ( in_array( $value[ $i ], $widget_ids ) ) {
+        					$side_bar_ids[] = $key;
+        				}
+        			}
+        		}
+        	}
+        	// Removing any repeating sidebar id.
+        	$side_bar_ids = array_unique( $side_bar_ids );
+        	?>
+
+            <!-- <h4><?php //_e( 'Lists of Text widgets using this form.', 'wpcf7sp' ); ?></h4> -->
+            <?php
+            if ( ! is_null( $wpcf7sp_text_widgets ) ): ?>
+                <table class="wpcf7sp" style="width: 50%;">
+                    <tbody>
+                    <tr>
+                        <th><?php _e( 'Widgets', 'wpcf7sp' ); ?></th>
+                    </tr>
+                    <?php
+                    foreach ( $side_bar_ids as $value ) {
+                        global $wp_registered_sidebars;
+                        if ( isset( $wp_registered_sidebars[ $value ] ) ) {
+                            echo '<tr><td>' . esc_html( $wp_registered_sidebars[ $value ]['name'] ) . '</td></tr>';
+                        }
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            else:
+                /******************************* if case should end here **********************************************/
+                ?>
+                <!-- <small><?php //_e( 'No Text widgets uses this form right now.', 'wpcf7sp' ); ?></small> -->
+        	<?php endif;
+        } else { ?>
+            <small><?php _e( 'No Text widgets uses this form right now.', 'wpcf7sp' ); ?></small>
+    <?php
+        }
+    ?>
+    </div>
+<?php }
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== Contact Form 7 - Show Page ===
+Contributors: ugene, sachyya-sachet
+Donate link: #
+Tags: Contact Form 7, form, forms, contactform7, contact form, find contact form, show page contact form 7, where contact form
+Requires at least: 4.5
+Tested up to: 4.8.3
+Stable tag: 1.0.2
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+A simple WordPress plugin that helps you to know which contact forms are used in the site.
+
+== Description ==
+You don't need this plugin but hey, you might need this to quickly check where the contact forms are used. If you have good memory then you will good without it. But there is chance to forget it right?
+
+So here below are the scenarios where you will find this plugin helpful:
+1. You have multiple contact forms used in various pages and posts or may be used in widgets. To edit the one you like meaning find it first. This plugin will help you showing the post/page name with a link to view or edit that post or page. For the widget case, it will tell you whether the current form is used in the widget or not.
+2. This is quite useful in this condition. Lets say you are a developer and you've come across the site where there are multiple contact forms. You might have to clean it up. This plugin will help you find and know if the contact form is used or not, meaning you are on your own far from the fear of mistakenly deleting the used one forms.
+
+By default this plugin only supports the search in default post types (post/page). But you can extend the search in custom post types of your own further by using a quick filter hook like below:
+
+`function extend_posttype_support ( $support ) {
+	return array( 'book', 'movie' );
+}
+add_filter( 'wpcf7sp_support_posttypes', 'extend_posttype_support' );`
+
+
+== Usages ==
+Backend
+
+	1. After installing the plugin, you will find a CF7 Show pages menu under each contact form edit page.
+	2. No settings, just view where the form is used. It wil list the name of post, pages in the table with a link to view and edit.
+	3. For other custom post types, filter is provided.
+
+= Notes =
+1. The plugin needs Contact Form 7 to be activated.
+
+== Installation ==
+1. Download and copy paste the files in the plugin folder of your WordPress.
+
+== Frequently Asked Questions ==
+
+= Do I have to have contact form 7 plugin? =
+
+Yes, this plugin is the extension to the famous Contact Form 7 plugin and you know CF7 is needed.
+
+== Upgrade Notice ==
+
+== Screenshots ==
+1. How it looks in the backend:
+
+== Changelog ==
+
+= 1.0.2 =
+* Add message when post types or widgets do not use the Contact Form.
+
+= 1.0.1 =
+* Extend plugin to search in widgets too but works only on Text Widget for now.
+* Remove default private post types.
+* Improve filter by checking for string and array only.
+
+= 1.0 =
+Initial Release
